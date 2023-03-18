cbe2cecb25f58850c46f2132cb0528e41792ae61
Paulo Scarano <scaranopaulo@gmail.com>
2023-03-18 12:21:51 -0300

    Montando um robô

--- js/main.js
+++ js/main.js
@@ -1,24 +1,16 @@
-const subtrair = document.querySelector("#subtrair");
-const somar = document.querySelector("#somar");
-const braco = document.querySelector("#braco");
-const controle = document.querySelectorAll(".controle-ajuste");
+const controle = document.querySelectorAll("[data-controle]");
 
 controle.forEach((elemento) => {
-  console.log(elemento);
+  elemento.addEventListener("click", (evento) => {
+    manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
+  });
 });
 
-somar.addEventListener("click", () => {
-  manipulaDados("somar");
-});
-
-subtrair.addEventListener("click", () => {
-  manipulaDados("subtrair");
-});
-
-function manipulaDados(operacao) {
-  if (operacao === "somar") {
-    braco.value = parseInt(braco.value) + 1;
+function manipulaDados(operacao, controle) {
+  const peca = controle.querySelector("[data-contador]");
+  if (operacao === "+") {
+    peca.value = parseInt(peca.value) + 1;
   } else {
-    braco.value = parseInt(braco.value) - 1;
+    peca.value = parseInt(peca.value) - 1;
   }
 }
